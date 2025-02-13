c53f83357ded8ab2efc2161283ba66966775717f
Jow-Sky <jonathan_zoz@estudante.sesisenai.org.br>
2025-02-13 17:21:09 -0300

    pão

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {Gi3dStairs,GiBeech,GiBee,GiBeachBall,GiBigWave,GiCamel,GiAxolotl,GiBanknote,GiAmbulance,GiBalloonDog,GiCapybara,GiCakeSlice,GiCommercialAirplane,GiConsoleController,GiCoffeeCup,GiFamilyHouse,GiHiking,GiLockedChest,GiMeal,GiPiggyBank,GiPresent} from "react-icons/gi";
+
  
  
  
@@ -9,17 +10,32 @@ import {Gi3dStairs,GiBeech,GiBee,GiBeachBall,GiBigWave,GiCamel,GiAxolotl,GiBankn
 
 function App() {
     const [randomNumber, setRandomNumber] = useState('')
+
+    const [botao, setbotao] = useState([])
+
     const [imagens, setImagens] = useState([<Gi3dStairs />,<GiBeech />,<GiBee />,<GiBeachBall />,<GiBigWave />,<GiCamel />,<GiAxolotl />,<GiBanknote />,<GiAmbulance />,<GiBalloonDog />,<GiCapybara />,<GiCakeSlice />,<GiCommercialAirplane />,<GiConsoleController />,<GiCoffeeCup />,<GiFamilyHouse />,<GiHiking />,<GiLockedChest />,<GiMeal />,<GiPiggyBank />,<GiPresent />])
-    const generateRandomNumber = () => {
-    const n = Math.floor(Math.random() * imagens.length) 
-    setRandomNumber(imagens [n])
-    setImagens(prev => prev.filter((_, index) => index !== n))
-    if (imagens.length === 0) {
+    
+    const [imagensClone, setImagensClone] = useState([...imagens])
+    
+    const gerarRandomNumber = () => {
+    
+    const n = Math.floor(Math.random() * imagensClone.length) 
+    
+    setRandomNumber(imagensClone [n])
+    
+    setImagensClone(prev => prev.filter((_, index) => index !== n))
+    if (imagensClone.length === 0) {
       alert("Todas as imagens já foram mostradas!")
       return
+     
+    }
+    
   }
-
-  }
+   const reset = () =>{
+      setRandomNumber(null)
+      setImagensClone([...imagens])
+   }
+  
 
   return (
 
@@ -29,7 +45,9 @@ function App() {
         </div>
         <div className="App">
          
-        <button onClick={generateRandomNumber}>Gerar imagen</button>
+         {imagensClone.length === 0 ? <button className = "botao-reset" onClick={reset}>reset</button>:<button className = "botao-gerar" onClick={gerarRandomNumber}>Gerar imagen</button>}
+  
+        
        <p>{randomNumber}</p>
         </div>
     </>
